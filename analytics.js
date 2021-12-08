ae32e675edb232b0c7463ae42d38afef1c6ecb16
Your Name <y.nkn.0628@gmail.com>
2021-12-08 20:22:11 +0900

    analytics

---
+++ analytics.js
@@ -0,0 +1,20 @@
+if(document.head){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-2M5YR0EYPS';
+    script.async = true;
+    document.head.appendChild(script);
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.innerHTML = ```
+    window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+
+            gtag("config", "G-2M5YR0EYPS");
+    
+    ```
+    document.head.appendChild(script);
+}
